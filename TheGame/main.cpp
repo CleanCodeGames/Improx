626a5bed45280c857dc9d6f684009c960387e503
vitaliy <cleancodegames@mail.ru>
2024-02-25 23:15:01 +0300

    добавил вращение гравитации мира

--- TheGame/main.cpp
+++ TheGame/main.cpp
@@ -11,19 +11,38 @@ int main()
 {
     System::Initialization();
     std::vector<b2Body*> boxes;
-    b2World world(b2Vec2(0.0f, -9.8f));
-    Utils_b2d::CreateStaticBox(world, sf::Vector2f(0, 200), sf::Vector2f(600, 25));
-    
+    b2Vec2 gravity(9.8f, 9.8f);
+    float32 gravity_factor = 0.f;
+    b2World world(gravity);
+    Utils_b2d::CreateStaticBox(world, sf::Vector2f(0, 300), sf::Vector2f(600, 25));
+    Utils_b2d::CreateStaticBox(world, sf::Vector2f(0, -300), sf::Vector2f(600, 25));
+    Utils_b2d::CreateStaticBox(world, sf::Vector2f(300, 0), sf::Vector2f(25, 600));
+    Utils_b2d::CreateStaticBox(world, sf::Vector2f(-300, 0), sf::Vector2f(25, 600));
 
-    sf::RectangleShape stayblock(sf::Vector2f(600, 25));
-    stayblock.setFillColor(sf::Color::Blue);
-    stayblock.setOrigin(stayblock.getSize() / 2.f);
-    stayblock.setPosition(sf::Vector2f(0, 200));
+    sf::RectangleShape stayblock[4] = { 
+        sf::RectangleShape(sf::Vector2f(600, 25)), 
+        sf::RectangleShape(sf::Vector2f(600, 25)), 
+        sf::RectangleShape(sf::Vector2f(25, 600)), 
+        sf::RectangleShape(sf::Vector2f(25, 600)) 
+    };
+
+    stayblock[0].setPosition(sf::Vector2f(0, 300));
+    stayblock[1].setPosition(sf::Vector2f(0, -300));
+    stayblock[2].setPosition(sf::Vector2f(300, 0));
+    stayblock[3].setPosition(sf::Vector2f(-300, 0));
+
+    for (auto& shape : stayblock)
+    {
+        shape.setFillColor(sf::Color::Blue);
+        shape.setOrigin(shape.getSize() / 2.f);
+    }
     
     while (System::window->isOpen()) 
     {
         System::Update();
-        System::camera->rotate(5.f * System::time_elapsed);
+        gravity_factor += System::time_elapsed;
+        world.SetGravity(b2Vec2(gravity.x * std::cosf(gravity_factor), gravity.y * std::sinf(gravity_factor)));
+        //System::camera->rotate(5.f * System::time_elapsed);
         System::window->setView(*System::camera);
         world.Step(System::time_elapsed, 8, 3);
         
@@ -51,7 +70,8 @@ int main()
         System::window->clear();
 
         Utils_b2d::UpdateDynamicBoxes(boxes);
-        System::window->draw(stayblock);
+        for (auto& shape : stayblock)
+            System::window->draw(shape);
         System::window->display();
     }
     return 0;
